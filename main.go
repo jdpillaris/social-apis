c188e96653cd75a6ce6b6e762758003ec7a17294
Jaidev <jdpilaris@gmail.com>
2018-08-28 07:25:36 +0800

    Add logs

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"app/routes"
+	"os"
 
 	"github.com/apex/log"
+	"github.com/apex/log/handlers/text"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
 
 func init() {
+	log.SetHandler(text.New(os.Stderr))
+
 	// Configure gin run mode
 	gin.SetMode(gin.ReleaseMode)
 }
