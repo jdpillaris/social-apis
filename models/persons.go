cdeb5a2ec69b175fac2c85d2d5a0338ae6729929
Jaidev <jdpilaris@gmail.com>
2018-08-29 10:00:31 +0800

    Add models

--- models/persons.go
+++ models/persons.go
@@ -1,1 +1,18 @@
 package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+// Person model
+type Person struct {
+	ID         int64     `json:"id"`
+	Email      string    `json:"email"`
+	CreatedAt  time.Time `json:"created_at"`
+}
+
+// Create a person
+func (p *Person) Create() (*Person, error) {
+	return p, nil
+}
--- models/relationships.go
+++ models/relationships.go
@@ -1,1 +1,45 @@
 package models
+
+import (
+	"time"
+)
+
+// Relationship model
+type Relationship struct {
+	ID         int64     `json:"id"`
+	Person1    int64     `json:"person_1"`
+	Person2    int64     `json:"person_2"`
+	IsFriends  bool    	 `json:"is_friends"`
+	Follows    bool    	 `json:"follows"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
+
+// Create a relationship
+func (r *Relationship) Create() (*Relationship, error) {
+	return r, nil
+}
+
+// Update relationship
+func (r *Relationship) Update(id int64) (*Relationship, error) {
+	return r, nil
+}
+
+// GetAll friends
+func (r *Relationship) GetAllFriends(UID int64) ([]Relationship, error) {
+	rs := make([]Relationship, 0)
+
+	return rs, nil
+}
+
+// GetAll followers
+func (r *Relationship) GetAllFollowers(UID int64) ([]Relationship, error) {
+	rs := make([]Relationship, 0)
+
+	return rs, nil
+}
+
+// Delete a specific relationship based on person_1, person_2 IDs
+func (r *Relationship) Delete(person_1, person_2 int64) error {
+	return nil
+}
